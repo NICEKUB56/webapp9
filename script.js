d1eafc3c2fec7ab8bf765f62438ca32f2a947d23
NICEKUB56 <seksantpama@gmail.com>
2025-05-22 11:47:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("เสกสันต์ ปะมะ")
+console.log("ชทส 2 11")
+console.log("แผนกเทคโนโลยีสารสนเทศ")
+console.log("114/47บางพรม เขตตลิ่งชัน กรุงเทพมหานคร")
+console.log("0800877029")
+console.log("seksantpama@gmail.com")
+console.log("นักแข่งROv")
+
+alert("หิวข้าววว");
+
+
+document.write("ผลรวมของ 10 + 12 = ", 10 + 12, "<br>");
+document.write("ผลรวมของ 20 - 12 = ", 20-12,);
